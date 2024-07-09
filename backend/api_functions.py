190299d6234e6522f13b4fcdcd34f8be8fdac42f
Vidyadhari43 <21cs01043@iitbbs.ac.in>
2024-07-09 19:52:14 +0530

    endpoints

--- backend/api_functions.py
+++ backend/api_functions.py
@@ -1,5 +1,6 @@
 import string
 import secrets
+from fastapi import WebSocket
 
 def GenerateRandomCode(current_list:list[str])->str:
     """
@@ -19,3 +20,16 @@ def GenerateRandomCode(current_list:list[str])->str:
             current_list.append(unique_code)
             break
     return unique_code
+
+#should try by changing it to synchronous and see how they differ
+
+async def broadcast_msg(data:str,users:dict[str,list[WebSocket]],unique_code:str)->None:
+    for socket in users[unique_code]:
+        await socket.send_text(data)
+
+async def user_exit(websocket,users,unique_code,current_list)->None:
+    await users[unique_code].remove(websocket)
+    await broadcast_msg('user left',users,unique_code)
+    if len(users[unique_code])==0:
+        del users[unique_code]
+        await current_list.remove(unique_code)
--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket,WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 
 import api_functions
 
 current_list:list[str]=[]
-
+users:dict[str,list[WebSocket]]=dict()
 app=FastAPI()
 
 app.add_middleware(
@@ -15,7 +15,7 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
-@app.get('/unique_code_generate')
+@app.get('/create_room/unique_code_generate') #should change it to post request. request should send username
 def unique_code_gen()->dict:
     """
     Creates the unique code which is not in use at present
@@ -24,4 +24,35 @@ def unique_code_gen()->dict:
         dict: json response for get request
     """
     unique_code:str=api_functions.GenerateRandomCode(current_list)
+    print(current_list)
     return {'unique_code':unique_code}
+
+@app.post('/join_room/enter_code')
+def join_room(unique_code:str,username:str)->dict:
+    
+    if unique_code not in current_list:
+        return {'status':'fail','msg':'Invalid Code'}
+    
+    else:
+        return {'status':'success','msg':'Valid Code'}
+
+
+@app.websocket('/enter_room')
+async def enter_room(websocket:WebSocket,unique_code:str)->None:
+    await websocket.accept()
+    users[unique_code].append(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await api_functions.broadcast_msg(data,users,unique_code)
+    except WebSocketDisconnect:
+        #remove from the list
+        await api_functions.user_exit(websocket,users,unique_code,current_list)
+    # return {'status':'disconnected'}
+    
+#should add an end point for leave button.
+        
+
+
+    
+    
